8159802f0a1cf9db72ee929baf187a3b30345b03
nezhar <harry@5c.ro>
2015-01-31 21:54:15 +0100

    Plugin Options Page

--- fb-pinwall.php
+++ fb-pinwall.php
@@ -30,6 +30,11 @@
 
 defined('ABSPATH') or die("No script kiddies please!");
 
-include("fbpw-tmp-conf.php");
+$options = get_option( 'fb-pinwall-options' );
+
+define('APP_ID', $options['facebook_app_key']);
+define('APP_SECRET', $options['facebook_app_secret']);
+define('FB_PAGES', $options['facebook_page']);
+
 include("fbpw-options.php");
 include("fbpw-shortcode.php");
--- fbpw-options.php
+++ fbpw-options.php
@@ -1,36 +1,161 @@
 <?php
 defined('ABSPATH') or die("No script kiddies please!");
 
-add_action( 'admin_menu', 'fbpw_options' );
+class MySettingsPage
+{
+    /**
+     * Holds the values to be used in the fields callbacks
+     */
+    private $options;
 
-function fbpw_options() {
-	add_options_page( 'Facebook Pinwall Options', 'FB Pinwall', 'manage_options', 'fbpw', 'plugin_options' );
+    /**
+     * Start up
+     */
+    public function __construct()
+    {
+        add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
+        add_action( 'admin_init', array( $this, 'page_init' ) );
+    }
+
+    /**
+     * Add options page
+     */
+    public function add_plugin_page()
+    {
+        // This page will be under "Settings"
+        add_options_page(
+            'Settings Admin', 
+            'FB Pinwall', 
+            'manage_options', 
+            'fb-pinwall-settings', 
+            array( $this, 'create_admin_page' )
+        );
+    }
+
+    /**
+     * Options page callback
+     */
+    public function create_admin_page()
+    {
+        // Set class property
+        $this->options = get_option( 'fb-pinwall-options' );
+        ?>
+        <div class="wrap">
+            <?php screen_icon(); ?>
+            <h2>FB Pinwall</h2>           
+            <form method="post" action="options.php">
+            <?php
+                // This prints out all hidden setting fields
+                settings_fields( 'fb-pinwall-option' );   
+                do_settings_sections( 'fb-pinwall-settings' );
+                submit_button(); 
+            ?>
+            </form>
+        </div>
+        <?php
+    }
+
+    /**
+     * Register and add settings
+     */
+    public function page_init()
+    {        
+        register_setting(
+            'fb-pinwall-option', // Option group
+            'fb-pinwall-options', // Option name
+            array( $this, 'sanitize' ) // Sanitize
+        );
+
+        add_settings_section(
+            'fb-api-settings', // ID
+            'FB API v2.x Settings', // Title
+            array( $this, 'print_section_info' ), // Callback
+            'fb-pinwall-settings' // Page
+        );  
+
+        add_settings_field(
+            'facebook_app_key', // ID
+            'Facebook App Key', // Title 
+            array( $this, 'facebook_app_key_callback' ), // Callback
+            'fb-pinwall-settings', // Page
+            'fb-api-settings' // Section           
+        );      
+
+        add_settings_field(
+            'facebook_app_secret', 
+            'Facebook App Secret', 
+            array( $this, 'facebook_app_secret_callback' ), 
+            'fb-pinwall-settings', 
+            'fb-api-settings'
+        );
+
+        add_settings_field(
+            'facebook_page', 
+            'Facebook Page for Feed', 
+            array( $this, 'facebook_page_callback' ), 
+            'fb-pinwall-settings', 
+            'fb-api-settings'
+        );    
+    }
+
+    /**
+     * Sanitize each setting field as needed
+     *
+     * @param array $input Contains all settings fields as array keys
+     */
+    public function sanitize( $input )
+    {
+        $new_input = array();
+        if( isset( $input['facebook_app_key'] ) )
+            $new_input['facebook_app_key'] = absint( $input['facebook_app_key'] );
+
+        if( isset( $input['facebook_app_secret'] ) )
+            $new_input['facebook_app_secret'] = sanitize_text_field( $input['facebook_app_secret'] );
+
+        if( isset( $input['facebook_page'] ) )
+            $new_input['facebook_page'] = sanitize_text_field( $input['facebook_page'] );
+
+        return $new_input;
+    }
+
+    /** 
+     * Print the Section text
+     */
+    public function print_section_info()
+    {
+        echo 'This Setting are used to define the connection to the facebook API in order to access a facebook page feed.';
+    }
+
+    /** 
+     * Get the settings option array and print one of its values
+     */
+    public function facebook_app_key_callback()
+    {
+        printf(
+            '<input type="text" id="facebook_app_key" name="fb-pinwall-options[facebook_app_key]" value="%s" />',
+            isset( $this->options['facebook_app_key'] ) ? esc_attr( $this->options['facebook_app_key']) : ''
+        );
+    }
+
+    /** 
+     * Get the settings option array and print one of its values
+     */
+    public function facebook_app_secret_callback()
+    {
+        printf(
+            '<input type="text" id="facebook_app_secret" name="fb-pinwall-options[facebook_app_secret]" value="%s" />',
+            isset( $this->options['facebook_app_secret'] ) ? esc_attr( $this->options['facebook_app_secret']) : ''
+        );
+    }
+
+    public function facebook_page_callback()
+    {
+        printf(
+            '<input type="text" id="facebook_page" name="fb-pinwall-options[facebook_page]" value="%s" />',
+            isset( $this->options['facebook_page'] ) ? esc_attr( $this->options['facebook_page']) : ''
+        );
+    }
 }
 
-function plugin_options() {
-	if ( !current_user_can( 'manage_options' ) )  {
-		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
-	}
-	echo '<div class="wrap">';
-	?>
-
-	<form method="post" action="options.php"> 
-		<?php
-		settings_fields( 'myoption-group' );
-		do_settings_sections( 'myoption-group' );
-		?>
-
-		<?php submit_button('Save Settings'); ?>
-	</form>
-
-	<?php
-	echo '</div>';
-}
-
-/*
-add_action( 'admin_init', 'fbpw_options_init');
-
-function fbpw_options_init(){
-
-}
-*/
+if( is_admin() )
+    $my_settings_page = new MySettingsPage();
